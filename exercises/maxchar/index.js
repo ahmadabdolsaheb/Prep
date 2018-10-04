cec02a7f4379f1da7350f7877cea73726f29ffbc
Ahmad Abdolsaheb <ahmad.abdolsaheb@gmail.com>
2018-10-04 19:23:32 +0300

    two solutions

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,38 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let obj = str.split("")
+  .reduce(function (accu, curr) {
+  accu[curr] = accu[curr] + 1 || 1
+  return accu;
+  }, {});
+
+  let top = [0, 0];
+
+  for( let item in obj){
+    if(obj[item]> top[1]){
+      top[0] = item;
+      top [1] = obj[item]
+    }
+  }
+  return top[0];
+}
 
 module.exports = maxChar;
+
+
+
+// let obj = {}
+// let top = [0, 0];
+// for( let item of str){
+//   obj[item] = obj[item] + 1 || 1;
+// }
+//
+// for( let item in obj){
+//   if(obj[item]> top[1]){
+//     top[0] = item;
+//     top [1] = obj[item]
+//   }
+// }
+// return top[0];
