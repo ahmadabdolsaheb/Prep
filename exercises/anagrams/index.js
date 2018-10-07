89728b592caba37439675b5164172d8c963b9873
Ahmad Abdolsaheb <ahmad.abdolsaheb@gmail.com>
2018-10-07 14:39:17 +0300

    2/5 answers

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,39 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+
+  const aCharMap = buildCharMap(stringA);
+  const bCharMap = buildCharMap(stringB);
+
+  if(Object.keys(aCharMap).length !== Object.keys(bCharMap).length){
+    return false;
+  }
+
+  for(let char in aCharMap){
+    if(aCharMap[char] !== bCharMap[char]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function buildCharMap(str){
+  const charMap = {};
+  for( let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+  return charMap;
+}
 
 module.exports = anagrams;
+
+// function anagrams(stringA, stringB) {
+//
+//   return sortAndFilter(stringA) === sortAndFilter(stringB);
+// }
+//
+// function sortAndFilter(str){
+//   return str.split('').filter(x => x.match(/\w/)).sort().toString();
+// }
