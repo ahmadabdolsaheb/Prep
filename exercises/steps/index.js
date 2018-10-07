73ac5f16fd543101f31f60a66565aef0356532aa
Ahmad Abdolsaheb <ahmad.abdolsaheb@gmail.com>
2018-10-07 17:18:11 +0300

    3 answers

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,41 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, row = 1) {
+  if (n < row){
+    return;
+  }
+  console.log("#".repeat(row)+" ".repeat(n-row));
+  steps(n, row + 1);
+}
 
 module.exports = steps;
+
+
+
+// function steps(n, row = 0, stair = '') {
+//   if (n == row){
+//     return;
+//   }
+//
+//   if (n === stair.length){
+//     console.log(stair);
+//     return steps(n, row+1);
+//
+//   }
+//
+//   if(stair.length <= row) {
+//     stair += '#';
+//   } else {
+//     stair += ' ';
+//   }
+//
+//   steps(n, row, stair);
+// }
+
+// function steps(n) {
+//
+//   for(let i = 1; i <= n; i++){
+//     console.log("#".repeat(i) + " ".repeat(n - i))
+//   }
+// }
