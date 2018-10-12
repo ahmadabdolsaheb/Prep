08ac6010d75ab5ab0bac11fcd2813ca712c43057
Ahmad Abdolsaheb <ahmad.abdolsaheb@gmail.com>
2018-10-12 21:24:24 +0300

    fibo 1 answer

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -9,14 +9,13 @@
 //   fib(4) === 3
 
 function fib(n) {
-  let arr = [];
-  let counter = 0;
-  let lastVal = 0;
-
-  while(n>counter){
-    arr.push[lastVal];
-      
+  const result = [0, 1];
+  for(let i = 2; i <= n; i++){
+    const a = result[i - 1];
+    const b = result[i - 2];
+    result.push(a + b);
   }
+  return result[n];
 }
 
 
